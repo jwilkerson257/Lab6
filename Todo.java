cfa01dca37dc20ffa5767739058dac9f81b3958a
jwilkerson257 <47432903+jwilkerson257@users.noreply.github.com>
2019-02-26 18:11:00 -0600

    Add files via upload

---
+++ Todo.java
@@ -0,0 +1,31 @@
+public class Todo {/*
+Lab Section_________: 13
+Last Modified_______: 2019-01-28
+OU ID_______________: 113406641
+OU EMAIL____________: Jwilkerson@ou.edu
+
+Tasks:
+1. 
+
+Estimated Time (minutes):
+1. 15
+2. 10
+3. 5
+4. 20
+5. 15
+6. 5
+7. 10
+8. 10
+9. 5
+
+Actual Time (minutes):
+1. 20
+2. 10
+3. 10
+4. 15
+5. 10
+6. 10
+7. 5
+8. 10
+9. 5
+*/}
