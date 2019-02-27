5dd2b1a43f1c193943d62a2bb6e6dbfb8f2b9c83
jwilkerson257 <47432903+jwilkerson257@users.noreply.github.com>
2019-02-26 19:39:26 -0600

    Update Todo.java

--- Todo.java
+++ Todo.java
@@ -5,27 +5,44 @@ OU ID_______________: 113406641
 OU EMAIL____________: Jwilkerson@ou.edu
 
 Tasks:
-1. 
+1. Finish Shape.java
+2. Sinish Polygon.java
+3. Create and finish Rectangle.java
+4. Create and finish Square.java
+5. Finish Trapezoid.java
+6. Finish EquilateralTriangle.java
+7. Finish Circle.java
+8. Finish ShapeSorter.java
+9. Finish ShapeAreaComparator.java
+10. Finish ShapePerimeterComparator.java
+11. Finish ShapeTest.java
+12. Finish ShapeSorterTest.java
 
 Estimated Time (minutes):
 1. 15
 2. 10
-3. 5
+3. 15
 4. 20
 5. 15
-6. 5
+6. 20
 7. 10
 8. 10
-9. 5
+9. 15
+10. 20
+11. 25
+12. 20
 
 Actual Time (minutes):
 1. 20
 2. 10
 3. 10
 4. 15
-5. 10
+5. 25
 6. 10
-7. 5
+7. 25
 8. 10
-9. 5
+9. 20
+10. 25
+11. 35
+12. 30
 */}
