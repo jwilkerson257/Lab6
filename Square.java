1d6e74ee163a7527eb27da89a22fdc46e7bcda5d
jwilkerson257 <47432903+jwilkerson257@users.noreply.github.com>
2019-02-26 18:09:24 -0600

    Update Square.java

--- Square.java
+++ Square.java
@@ -4,6 +4,9 @@ public class Square extends Rectangle
 
 	public Square(String id, double size)
 	{
+		/* The square constructor uses the rectangle constructor by making the two different sides 
+		 * the same length and uses the methods of the rectangle class instead of overriding them 
+		 */
 		super(id, size, size);
 	}
 	
